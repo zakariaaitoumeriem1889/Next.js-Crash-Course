ac2b3bce9cb09252de9c260b8b3f24ea84761b37
Zakaria AITOUMERIEM <zakariaaitoumeriem1889@gmail.com>
2021-01-27 21:22:52 +0100

    Custom Meta Component

--- pages/article/[id]/index.js
+++ pages/article/[id]/index.js
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import {useRouter} from "next/router";
 import {server} from "../../../config";
+import Meta from "../../../components/Meta";
 
 const article = ({article}) => {
     // const router = useRouter();
@@ -8,40 +9,40 @@ const article = ({article}) => {
 
     return (
         <>
-            <h1>{article.id}</h1>
+            <Meta title={article.title} description={article.excerpt}/>
+            <h1>{article.title}</h1>
             <p>{article.body}</p>
+            <br/>
             <Link href="/">Go Back</Link>
         </>
     );
 };
 
 export const getStaticProps = async (context) => {
-    const res = await fetch(
-        `${server}/api/articles/${context.params.id}`
-    )
+    const res = await fetch(`${server}/api/articles/${context.params.id}`);
 
-    const article = await res.json()
+    const article = await res.json();
 
     return {
         props: {
             article,
         },
-    }
-}
+    };
+};
 
 export const getStaticPaths = async () => {
-    const res = await fetch(`${server}/api/articles`)
+    const res = await fetch(`${server}/api/articles`);
 
-    const articles = await res.json()
+    const articles = await res.json();
 
-    const ids = articles.map((article) => article.id)
-    const paths = ids.map((id) => ({ params: { id: id.toString() } }))
+    const ids = articles.map((article) => article.id);
+    const paths = ids.map((id) => ({params: {id: id.toString()}}));
 
     return {
         paths,
         fallback: false,
-    }
-}
+    };
+};
 
 // export const getStaticProps = async (context) => {
 //   const res = await fetch(
