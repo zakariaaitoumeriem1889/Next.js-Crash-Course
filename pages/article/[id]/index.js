4b1f5e0e4dc842c3882fea5642ddce6de867a41c
Zakaria AITOUMERIEM <zakariaaitoumeriem1889@gmail.com>
2021-01-27 20:48:35 +0100

    getServerSideProps()

--- pages/article/[id]/index.js
+++ pages/article/[id]/index.js
@@ -1,10 +1,31 @@
+import Link from "next/link";
 import {useRouter} from "next/router";
 
-const article = () => {
-    const router = useRouter();
-    const {id} = router.query;
+const article = ({article}) => {
+    // const router = useRouter();
+    // const {id} = router.query;
 
-    return <div>This is an article</div>;
+    return (
+        <>
+            <h1>{article.id}</h1>
+            <p>{article.body}</p>
+            <Link href='/'>Go Back</Link>
+        </>
+    )
+};
+
+export const getServerSideProps = async (context) => {
+    const res = await fetch(
+        `https://jsonplaceholder.typicode.com/posts/${context.params.id}`
+    );
+
+    const article = await res.json();
+
+    return {
+        props: {
+            article
+        }
+    }
 };
 
 export default article;
