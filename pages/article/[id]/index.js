504bc9ccd8f4836e34e08b6884da2d4ad85bc9e4
Zakaria AITOUMERIEM <zakariaaitoumeriem1889@gmail.com>
2021-01-27 20:59:07 +0100

    getStaticPaths()

--- pages/article/[id]/index.js
+++ pages/article/[id]/index.js
@@ -9,23 +9,37 @@ const article = ({article}) => {
         <>
             <h1>{article.id}</h1>
             <p>{article.body}</p>
-            <Link href='/'>Go Back</Link>
+            <Link href="/">Go Back</Link>
         </>
-    )
-};
-
-export const getServerSideProps = async (context) => {
-    const res = await fetch(
-        `https://jsonplaceholder.typicode.com/posts/${context.params.id}`
     );
-
-    const article = await res.json();
-
-    return {
-        props: {
-            article
-        }
-    }
 };
+
+export const getStaticProps = async (context) => {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${context.params.id}`
+  )
+
+  const article = await res.json()
+
+  return {
+    props: {
+      article,
+    },
+  }
+}
+
+export const getStaticPaths = async () => {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts`)
+
+  const articles = await res.json()
+
+  const ids = articles.map((article) => article.id)
+  const paths = ids.map((id) => ({ params: { id: id.toString() } }))
+
+  return {
+    paths,
+    fallback: false,
+  }
+}
 
 export default article;
