cfa8e0d661570a84d6e08ebdb38590c1f9916a69
Zakaria AITOUMERIEM <zakariaaitoumeriem1889@gmail.com>
2021-01-27 19:47:28 +0100

    Create a Header

---
+++ components/Header.js
@@ -0,0 +1,11 @@
+const Header = () => {
+    return (
+        <div>
+            <h1>
+                <span>WebDev</span> News
+            </h1>
+        </div>
+    );
+}
+
+export default Header;
