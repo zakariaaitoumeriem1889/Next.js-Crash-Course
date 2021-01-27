2664bb84195ddfb80370f23b3768700e855282e1
Zakaria AITOUMERIEM <zakariaaitoumeriem1889@gmail.com>
2021-01-27 19:27:25 +0100

    Head

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,10 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <div>
+        <Head>
+            <title>WebDev Newz</title>
+            <meta name="keywords" content="web development, programming" />
+        </Head>
       <h1>Welcome to Next</h1>
     </div>
   )
