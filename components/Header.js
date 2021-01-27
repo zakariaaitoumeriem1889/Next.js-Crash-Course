adf53467c5b628be037cf80b6f11a6f7f30071f9
Zakaria AITOUMERIEM <zakariaaitoumeriem1889@gmail.com>
2021-01-27 19:52:05 +0100

    Styled JSX

--- components/Header.js
+++ components/Header.js
@@ -1,11 +1,16 @@
+import headerStyles from "../styles/Header.module.css";
+
 const Header = () => {
     return (
         <div>
-            <h1>
+            <h1 className={headerStyles.title}>
                 <span>WebDev</span> News
             </h1>
+            <p className={headerStyles.description}>
+                Keep up to date with latest web dev news
+            </p>
         </div>
     );
-}
+};
 
 export default Header;
